140671eccbebd1e1e3310af42b2bf21be646c0e9
warlok38 <warlok38@mail.ru>
2021-03-13 19:33:53 +0700

    пробую подключить index html

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React ТЕСТОВАЯ СТРАНИЦА УРА
+          Learn React ТЕСТОВАЯ СТРАНИЦА УРА!
         </a>
       </header>
     </div>
